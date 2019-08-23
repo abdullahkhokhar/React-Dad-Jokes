570acfd2cd6e2d3a818582fa570987af0ec803ee
Abdullah Khokhar <abdullah.khokhar@mail.utoronto.ca>
2019-08-23 18:14:30 -0400

    joke component has been created

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
+import Joke from './Joke';
 import axios from 'axios';
+import uuid from 'uuid/v4'
 import './JokeList.css';
 
 class JokeList extends Component {
@@ -22,10 +24,19 @@ class JokeList extends Component {
       let res = await axios.get("https://icanhazdadjoke.com/", {
         headers: { Accept: "application/json"}
       });
-
-      jokes.push(res.data.joke);
+      // push in an an object to the array, each object has a joke and a vote
+      jokes.push({id: uuid(), text: res.data.joke, votes: 0});
     }
     this.setState({jokes: jokes}); // overwrite the old state
+  }
+
+  handleVote(id, delta){
+    this.setState(
+      st => ({
+        jokes: st.jokes.map(j =>
+          j.id === id ? {...j, votes: j.votes + delta} : j)
+      })
+    )
   }
 
   render(){
@@ -39,7 +50,13 @@ class JokeList extends Component {
 
         <div className = "JokeList-jokes">
           {this.state.jokes.map(j => (
-            <div>{j}</div>
+            <Joke
+              key={j.id}
+              votes={j.votes}
+              text={j.text}
+              upvote = {() => this.handleVote(j.id, 1)}
+              downvote = {() => this.handleVote(j.id, -1)}
+            />
           ))}
         </div>
       </div>
