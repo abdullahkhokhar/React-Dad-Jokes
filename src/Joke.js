611af16a1c0358a0b7bff676ee41f9f6c4e60d0b
Abdullah Khokhar <abdullah.khokhar@mail.utoronto.ca>
2019-08-27 14:09:23 -0400

    styling of voting wheel and emoji updating completed

--- src/Joke.js
+++ src/Joke.js
@@ -20,12 +20,31 @@ class Joke extends Component{
     }
   }
 
+  getColor() {
+    if (this.props.votes >= 15) {
+      return "#4CAF50";
+    } else if (this.props.votes >= 12) {
+      return "#8BC34A";
+    } else if (this.props.votes >= 9) {
+      return "#CDDC39";
+    } else if (this.props.votes >= 6) {
+      return "#FFEB3B";
+    } else if (this.props.votes >= 3) {
+      return "#FFC107";
+    } else if (this.props.votes >= 0) {
+      return "#FF9800";
+    } else {
+      return "#f44336";
+    }
+  }
+
   render(){
     return(
       <div className = "Joke">
         <div className = "Joke-buttons">
           <i className = "fas fa-arrow-up" onClick = {this.props.upvote}/>
-          <span className = "Joke-votes">{this.props.votes}</span>
+          <span className = "Joke-votes" style={{ borderColor: this.getColor() }}>
+          {this.props.votes}</span>
           <i className = "fas fa-arrow-down" onClick = {this.props.downvote}/>
         </div>
 
